8cd02fab8989d945c945da386fe7f76a64e2aab1
sanjayarun2 <eee2015arunkumar.kn15@gmail.com>
2024-12-01 16:22:11 +0000

    update47

--- _app.js
+++ _app.js
@@ -1,7 +1,7 @@
 // _app.js
 import { DefaultSeo } from 'next-seo';
 import SEOConfig from '../next-seo.config';
-import Head from 'next/head';
+import Script from 'next/script';
 
 function MyApp({ Component, pageProps }) {
   return (
@@ -10,35 +10,38 @@ function MyApp({ Component, pageProps }) {
       <DefaultSeo {...SEOConfig} />
 
       {/* JSON-LD Schema for Local Business */}
-      <Head>
-        <script type="application/ld+json">
-          {`
+      <Script
+        id="json-ld"
+        type="application/ld+json"
+        strategy="beforeInteractive"
+        dangerouslySetInnerHTML={{
+          __html: `
           {
             "@context": "http://schema.org",
             "@type": "Store",
             "name": "Elampillai Sarees",
             "address": {
               "@type": "PostalAddress",
-              "streetAddress": "Elampillai",  // Replace with your address
+              "streetAddress": "Elampillai",
               "addressLocality": "Elampillai",
               "addressRegion": "Tamil Nadu",
               "postalCode": "637502",
               "addressCountry": "IN"
             },
-            "telephone": "+91-XXXX-XXXXXX",  // Replace with your phone number
+            "telephone": "+91-XXXX-XXXXXX",
             "url": "https://www.elampillai.in",
             "sameAs": [
-              "https://www.facebook.com/elampillai",  // Optional: Add your social media links
+              "https://www.facebook.com/elampillai",
               "https://www.instagram.com/elampillai"
             ],
-            "openingHours": "Mo-Sa 09:00-18:00",  // Replace with your store hours
-            "priceRange": "$$",  // Optional: Add price range
-            "image": "https://www.elampillai.in/images/logo.jpg",  // Optional: Add image
+            "openingHours": "Mo-Sa 09:00-18:00",
+            "priceRange": "$$",
+            "image": "https://www.elampillai.in/images/logo.jpg",
             "description": "Visit Elampillai Sarees for premium soft silk, pattu, and cotton sarees in Elampillai."
           }
-          `}
-        </script>
-      </Head>
+          `,
+        }}
+      />
 
       {/* Render the page content */}
       <Component {...pageProps} />
